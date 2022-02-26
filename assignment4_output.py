b28b9677e451d50afdd10da6214499be028b2800
ShreyaEE <96696430+ShreyaEE@users.noreply.github.com>
2022-02-27 01:15:28 +0530

    Add files via upload

---
+++ assignment4_output.py
@@ -0,0 +1,72 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+======================= RESTART: D:\assignment4_input.py =======================
+Q-1
+Move disk 1 from rod A to rod C
+Move disk 2 from rod A to rod B
+Move disk 1 from rod C to rod B
+Move disk 3 from rod A to rod C
+Move disk 1 from rod B to rod A
+Move disk 2 from rod B to rod C
+Move disk 1 from rod A to rod C
+
+
+Q-2
+Enter the number of rows: 5
+USING LOOPS
+      1 
+     1 1 
+    1 2 1 
+   1 3 3 1 
+  1 4 6 4 1 
+
+
+USING RECURSSIONS
+        1   
+      1   1   
+    1   2   1   
+  1   3   3   1   
+1   4   6   4   1   
+
+
+Q-3
+Enter first number: 72
+Enter second number: 5
+(A) Checking if the quotient and remainder is a callable value or not.
+False
+False
+(B)
+All the values are non zero
+(C): Numbers that are greater than 4 are : [18, 6, 7, 19, 7, 20, 8]
+(D): Set: {6, 7, 8, 18, 19, 20}
+(E): Immutable set: frozenset({18, 19, 20, 6, 7, 8})
+(F): Hash value of the max value from the above set: 20
+
+
+Q-4
+Student details are here..
+The name of the student is Shreya and roll number is 21104108
+The object is destroyed
+
+
+Q-5
+ Name of the Employee: Mehak
+        Salary: 40000
+ Name of the Employee: Ashok
+        Salary: 50000
+ Name of the Employee: Viren
+        Salary: 60000
+(a)
+****Updated salary of Mehak****
+ Name of the Employee: Mehak
+        Salary: 70000
+(b)
+The record of viren is deleted
+
+
+Q-6
+Enter the word: hindustan
+Enter a new meaningful word using the exact same letters to test your friendship: stanhindu
+Does the word makes sense?(y or n): n
+The word doesn't have a meaning, friendship is fake
